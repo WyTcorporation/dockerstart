ca1f9ef43f75fd85ea6db61862164c36f26c7e52
WyTcorporation <wytcorporation@gmail.com>
2021-10-12 10:24:15 +0300

    Start

---
+++ www/hello.test/index.php
@@ -0,0 +1,14 @@
+
+<?php
+/**
+ * Create by: Vladislav Gladyr
+ * Site: lockit.com.ua
+ * Email: wild.savedo@gmail.com
+ * Date : 12.10.2021
+ * Time: 06:50
+ * User: WyTcorporation, WyTcorp, WyTco
+ */
+
+phpinfo();
+
+?>
